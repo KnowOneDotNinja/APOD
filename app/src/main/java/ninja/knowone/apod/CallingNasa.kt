1aae75f9919b4697272fe907e90647f82afa5e58
Andrew Hughes <social@hugheswd.com>
2019-04-21 18:56:39 -0700

    Matching things up

--- app/src/main/java/ninja/knowone/apod/CallingNasa.kt
+++ app/src/main/java/ninja/knowone/apod/CallingNasa.kt
@@ -8,10 +8,11 @@ import android.widget.Toast
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import java.lang.Exception
 
 class CallingNasa(private val activity: Activity) {
 
-    fun picSnag(addyPasser: (String) -> Unit): String {
+    fun picSnag(addyPasser: (String) -> Unit) {
         val client = OkHttpClient()
         val url = "https://api.nasa.gov/planetary/apod?api_key=${R.string.api_key}&hd=true"
         val request: Request = Request.Builder().url(url).build()
@@ -22,10 +23,13 @@ class CallingNasa(private val activity: Activity) {
 
             if (response.isSuccessful) {
                 activity.runOnUiThread {
-                    //val myText= response.body().toString()
-                    val myThing = JSONObject(response.body().toString())
-                    val addy = myThing.getString("hdurl")
-                    addyPasser(addy)
+                    try {
+                        val myThing = JSONObject(response.body().toString())
+                        val addy = myThing.getString("hdurl")
+                        addyPasser(addy)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
                 }
             } else {
                 Toast.makeText(activity, "Response Unsuccessful", Toast.LENGTH_LONG).show()
--- app/src/main/java/ninja/knowone/apod/MainActivity.kt
+++ app/src/main/java/ninja/knowone/apod/MainActivity.kt
@@ -3,6 +3,7 @@ package ninja.knowone.apod
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.net.URL
 
@@ -12,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageAddy = CallingNasa(this).picSnag { addy ->  }
-        Glide.with(this).
+        CallingNasa(this).picSnag { addy ->
+            Glide.with(this).load(addy).into(ivMain)
+        }
+//        Glide.with(this)./
     }
 }

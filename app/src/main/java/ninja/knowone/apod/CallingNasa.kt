d8d2498131308309ffd37d2c2366950dec2f3561
Trigger Mike <triggermikedj@gmail.com>
2019-04-21 19:43:15 -0700

    Achieved functionality

--- app/src/main/java/ninja/knowone/apod/CallingNasa.kt
+++ app/src/main/java/ninja/knowone/apod/CallingNasa.kt
@@ -2,18 +2,17 @@ package ninja.knowone.apod
 
 import android.app.Activity
 import android.os.AsyncTask
-import android.support.annotation.MainThread
-import android.widget.TextView
 import android.widget.Toast
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
+import java.lang.Exception
 
 class CallingNasa(private val activity: Activity) {
 
-    fun picSnag(addyPasser: (String) -> Unit): String {
+    fun picSnag(addyPasser: (String) -> Unit) {
         val client = OkHttpClient()
-        val url = "https://api.nasa.gov/planetary/apod?api_key=${R.string.api_key}&hd=true"
+        val url = "https://api.nasa.gov/planetary/apod?api_key=${activity.getString(R.string.api_key)}&hd=true"
         val request: Request = Request.Builder().url(url).build()
 
         AsyncTask.execute {
@@ -21,18 +20,16 @@ class CallingNasa(private val activity: Activity) {
             val response = client.newCall(request).execute()
 
             if (response.isSuccessful) {
+                val myThing = JSONObject(response.body()?.string())
+                val addy = myThing.getString("hdurl")
                 activity.runOnUiThread {
-                    //val myText= response.body().toString()
-                    val myThing = JSONObject(response.body().toString())
-                    val addy = myThing.getString("hdurl")
-                    addyPasser(addy)
+                        addyPasser(addy)
                 }
             } else {
-                Toast.makeText(activity, "Response Unsuccessful", Toast.LENGTH_LONG).show()
+                activity.runOnUiThread() {
+                    Toast.makeText(activity, "Response Unsuccessful", Toast.LENGTH_LONG).show()
+                }
             }
         }
-
-        //val myThing: JSONObject =
-
     }
 }

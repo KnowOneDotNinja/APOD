37619f771a73d046747253fa805173c114ac3069
Michael W <michael@knowone.ninja>
2019-06-16 01:44:35 -0700

    Cancelled video support

--- app/src/main/java/ninja/knowone/apod/APODMainFragment.kt
+++ app/src/main/java/ninja/knowone/apod/APODMainFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_apod_main.*
 import java.text.SimpleDateFormat
@@ -58,7 +59,7 @@ class APODMainFragment: Fragment() {
                     Glide.with(this).load(myThing.getString("hdurl")).into(ivMain)
                 }
                 myThing.get("media_type") == "video" -> {
-                    // Do thing with video
+                    Toast.makeText(activity, "Video is not supported :(", Toast.LENGTH_LONG).show()
                 }
                 else -> Glide.with(this).load(context?.getDrawable(R.drawable.no_vid)).into(ivMain)
             }
